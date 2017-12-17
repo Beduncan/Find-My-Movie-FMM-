2302dbd3f5d31f24d3c98af5e5ba88b50fbdf582
Donnyv116 <donnyv116@gmail.com>
2017-12-16 22:34:01 -0700

    google api script added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -80,6 +80,8 @@ function dataHandler(data) {
 
 
 // gets yt trailer
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+
 $(function() {
     $('#Trailerbutton').on("click", function(e) {
        e.preventDefault();
@@ -87,30 +89,37 @@ $(function() {
        var request = gapi.client.youtube.search.list({
             part: "snippet",
             type: "video",
-            q: encodeURIComponent($('#movie-input').val()).replace(/%20/g, "+"),
+            q: encodeURIComponent($("#movie-input").val()).replace(/%20/g, "+"),
             maxResults: 3,
-            order: "viewCount",           
+            order: "viewCount",
+            publishedAfter: "2015-01-01T00:00:00Z"
        }); 
        // execute the request
        request.execute(function(response) {
           var results = response.result;
-          
+          $("#DonnysApi").html("");
           $.each(results.items, function(index, item) {
-          
-                $("#DonnysApi").append(item.snippet.title + " " + item.id.videoId + '<br>');
+            $.get("movie.html", function(data) {
+                $("#DonnysApi").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
             });
           });
+          resetVideoHeight();
        });
-    });   
+    });
+    
+    $(window).on("resize", resetVideoHeight);
+});
 
-
-
-
+function resetVideoHeight() {
+    $(".video").css("height", $("#DonnysApi").width() * 9/16);
+}
 
 function init() {
-    gapi.client.setApiKey("AIzaSyA6xKN2rb-KRc4KYNt0YHe4z0a8PqAP_MM");
+    gapi.client.setApiKey("AIzaSyCasvqhF4Adlvc5LlOY9S31JzUKZppoggo");
     gapi.client.load("youtube", "v3", function() {
         // yt api is ready
     });
 }
 
+
+// AIzaSyCasvqhF4Adlvc5LlOY9S31JzUKZppoggo
