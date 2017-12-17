b9d19ba9b4da0f4eee562980b252e76bc6b9d39c
Donnyv116 <donnyv116@gmail.com>
2017-12-16 19:36:22 -0700

    trying to get yt to return something

---
+++ assets/javascript/app.js
@@ -0,0 +1,116 @@
+var config = {
+        apiKey: "AIzaSyB6Ff461GnlfJfCqGsLEseAidIfh3jX8No",
+        authDomain: "find-my-movie-cfebd.firebaseapp.com",
+        databaseURL: "https://find-my-movie-cfebd.firebaseio.com",
+        projectId: "find-my-movie-cfebd",
+        storageBucket: "find-my-movie-cfebd.appspot.com",
+        messagingSenderId: "447183757316"
+        };
+        firebase.initializeApp(config);  
+        //var for database
+        var database = firebase.database();
+//global values
+var email = "";
+    //on click functions to save email
+    $("#emailbutton").on("click", function() {
+        event.preventDefault();
+        email = $("#email-input").val().trim();
+        console.log(email);
+        database.ref().push({
+            email: email,
+        });
+    });
+    //firebase watcher
+    database.ref().on("child_added", function(snapshot){
+        console.log(snapshot.val().email);
+    });
+    
+$("#movie-input").on("click", function(event) {
+   
+ event.preventDefault();
+ 
+var apikey = "xwnuct9pwa826d3qjnruj2h2";
+var baseUrl = "http://data.tmsapi.com/v1.1";
+var showtimesUrl = baseUrl + '/movies/showings';
+var zipCode = "85234";
+var d = new Date();
+
+var today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
+
+    $(document).ready(function() {
+        // send off the query
+        $.ajax({
+            url: showtimesUrl,
+                data: { startDate: today,
+                        zip: zipCode,
+                        jsonp: "dataHandler",
+                        api_key: apikey
+                    },          
+                dataType: "jsonp",
+            });
+
+         });
+});
+
+// callback to handle the results
+function dataHandler(data) {
+  
+    $("#AdamsApi").append('<p>Found ' + data.length + ' movies showing within 5 miles of ' + zipCode+':</p>');
+
+    var movies = data.hits;
+
+    $.each(data, function(index, movie) {
+
+        var movieData = '<div class="tile"><img src="http://developer.tmsimg.com/' + movie.preferredImage.uri + '?api_key='+apikey+'"><br/>';
+
+        movieData += movie.title;
+
+        if (movie.ratings) { movieData += ' (' + movie.ratings[0].code + ') </div>' };
+
+    $(document.body).append(movieData);
+
+  });
+}
+
+
+
+
+
+
+
+
+// gets yt trailer
+$(function() {
+    $('#Trailerbutton').on("click", function(e) {
+       e.preventDefault();
+       // prepare the request
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($('#movie-input').val()).replace(/%20/g, "+"),
+            maxResults: 3,
+            order: "viewCount",           
+       }); 
+       // execute the request
+       request.execute(function(response) {
+          var results = response.result;
+          
+          $.each(results.items, function(index, item) {
+          
+                $("#DonnysApi").append(item.snippet.title + " " + item.id.videoId + '<br>');
+            });
+          });
+       });
+    });   
+
+
+
+
+
+function init() {
+    gapi.client.setApiKey("AIzaSyA6xKN2rb-KRc4KYNt0YHe4z0a8PqAP_MM");
+    gapi.client.load("youtube", "v3", function() {
+        // yt api is ready
+    });
+}
+
